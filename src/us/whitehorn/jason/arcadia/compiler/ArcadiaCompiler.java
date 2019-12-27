68cb08ed5ed050605d7a4429bd068be1dce820af
Jason Whitehorn <jason.whitehorn@gmail.com>
2019-12-27 13:37:01 -0600

    first test

--- src/us/whitehorn/jason/arcadia/compiler/ArcadiaCompiler.java
+++ src/us/whitehorn/jason/arcadia/compiler/ArcadiaCompiler.java
@@ -1,11 +1,17 @@
 package us.whitehorn.jason.arcadia.compiler;
 import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import us.whitehorn.jason.arcadia.core.ArcadiaProgram;
 
 public class ArcadiaCompiler {
+
+    public static ArcadiaProgram compile(String src) throws InstantiationException, IllegalAccessException {
+        CharStream input = CharStreams.fromString(src);
+        return compile(input);
+    }
 
     public static ArcadiaProgram compile(CharStream src) throws InstantiationException, IllegalAccessException {
         ArcadiaLexer lexer = new ArcadiaLexer(src);
--- tests/ArcadiaListenerImplTests.java
+++ tests/ArcadiaListenerImplTests.java
@@ -1,13 +1,23 @@
 import org.junit.Test;
+import us.whitehorn.jason.arcadia.compiler.ArcadiaCompiler;
+import us.whitehorn.jason.arcadia.core.ArcadiaProgram;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
 
 public class ArcadiaListenerImplTests {
 
     @Test
-    public void testNothing() {
-        int n = 1;
+    public void testCanCompileNothing() {
+        ArcadiaProgram prog = null;
+        try {
+            prog = ArcadiaCompiler.compile("");
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
 
-        assertEquals(1, n);
+        assertNotNull(prog);
     }
 }
